ae06cd6ab2c16a8d89f4babc523a0e5496fcc68e
Seb James <sebsta@gmail.com>
2015-10-04 18:32:25 +0100

    Test fixtures

---
+++ chesschallenge/tests/fixtures.py
@@ -0,0 +1,57 @@
+test1_boards = [
+    [['K', None, 'K'], [None, None, None], [None, 'R', None]],
+    [['K', None, None], [None, None, 'R'], ['K', None, None]],
+    [[None, None, 'K'], ['R', None, None], [None, None, 'K']],
+    [[None, 'R', None], [None, None, None], ['K', None, 'K']],
+]
+
+test2_boards = [
+    [
+        [None, 'N', None, 'N'],
+        [None, None, 'R', None],
+        [None, 'N', None, 'N'],
+        ['R', None, None, None]
+    ],
+    [
+        [None, 'N', None, 'N'],
+        ['R', None, None, None],
+        [None, 'N', None, 'N'],
+        [None, None, 'R', None]
+    ],
+    [
+        ['R', None, None, None],
+        [None, 'N', None, 'N'],
+        [None, None, 'R', None],
+        [None, 'N', None, 'N']
+    ],
+    [
+        [None, None, 'R', None],
+        [None, 'N', None, 'N'],
+        ['R', None, None, None],
+        [None, 'N', None, 'N']
+    ],
+    [
+        [None, 'R', None, None],
+        ['N', None, 'N', None],
+        [None, None, None, 'R'],
+        ['N', None, 'N', None]
+    ],
+    [
+        [None, None, None, 'R'],
+        ['N', None, 'N', None],
+        [None, 'R', None, None],
+        ['N', None, 'N', None],
+    ],
+    [
+        ['N', None, 'N', None],
+        [None, None, None, 'R'],
+        ['N', None, 'N', None],
+        [None, 'R', None, None],
+    ],
+    [
+        ['N', None, 'N', None],
+        [None, 'R', None, None],
+        ['N', None, 'N', None],
+        [None, None, None, 'R'],
+    ]
+]
