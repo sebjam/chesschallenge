8d4e94c04fd1ea7f517ed12108c5a8ae0fe6fe8f
Seb James <sebsta@gmail.com>
2015-10-05 16:58:26 +0100

    Initial methods to place pieces on boards and refresh positions

--- chesschallenge/main.py
+++ chesschallenge/main.py
@@ -8,3 +8,21 @@ class ChessChallenge(object):
         self.width = width
         self.height = height
         self.pieces = pieces
+        self._place_pieces(pieces)
+
+    def _place_pieces(self, pieces, board=None, unavailable_positions=None):
+        if not unavailable_positions:
+            unavailable_positions = self._refresh_positions(
+                self.empty_board(self.width, self.height),
+            )
+
+        if not board:
+            board = self.empty_board()
+
+        # TODO: Place pieces on board and append to valid_boards
+
+    def _refresh_positions(
+            self, unavailable_positions, piece=None, y=None, x=None):
+        # TODO: Given the piece make the x,y unavailable and then calculate the
+        # potential hit positions and make them unavailable.
+        return unavailable_positions
